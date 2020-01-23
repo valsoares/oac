9df2aa937b97f317b1386e49a7ff04f621596e8c
Kakomo <kakomo2@gmail.com>
2020-01-23 18:04:52 -0300

    Rename Pseudo2.asm to 3.2]ORDENA(V).asm

--- Laboratório1/3.2]ORDENA(V).asm
+++ Laboratório1/3.2]ORDENA(V).asm
@@ -5,8 +5,8 @@ coordenadas: .word
 
 .text
 li a2,N
-la t2,coordenadas #peguei o endereço do vetor onde vou guardar as words coordenadas
-sw a2,0(t2) #primeiro elemento é o número de elementos no vetor coordenadas
+la t2,coordenadas #peguei o endereÃ§o do vetor onde vou guardar as words coordenadas
+sw a2,0(t2) #primeiro elemento Ã© o nÃºmero de elementos no vetor coordenadas
 
 #CODIGO DA VAL
 
@@ -18,11 +18,11 @@ slli t1,a0,16 #levar o numero para os 4 primeiros numeros de uma word. Ex: 0x444
 
 li a1,239 #limite superior da escolha do inteiro aleatorio de y
 ecall #fez o random do y em a0. Ex: 0x00005555
-or a0,a0,t1 #operação or(x+y) em a0. Ex: 0x44445555
+or a0,a0,t1 #operaÃ§Ã£o or(x+y) em a0. Ex: 0x44445555
 sw a0,4(t2) #armazenamento da coordenada em uma word
 addi t2,t2,4 #incrementa 4 em t2 para o armazenamento da proxima word
 addi t0,t0,1 #incrementa contador de numeros sorteados
-blt t0,a2,sorteia #t0 é o contador de numeros sorteados: if(t0<N) 
+blt t0,a2,sorteia #t0 Ã© o contador de numeros sorteados: if(t0<N) 
 
 #CODIGO XANDINHO
 
@@ -30,7 +30,7 @@ li t5, 4 #alocando um numero em t5
 mul a3,a2,t5 #multiplicamos o Nx4
 sub t2,t2,a3 #  voltando ao inicio do vetor de tamanho N ( t4 = Nx4)
 mv t0,zero
-#Codigo que faz a serapa��o de x e y no vetor principal
+#Codigo que faz a serapação de x e y no vetor principal
 VECTOR:
 lw t1,4(t2) #primeira coordenada
 li t3, 0xFFFF0000 #mascara do x
@@ -40,11 +40,11 @@ srli t3,t3,16
 fcvt.s.w f1,t3 #f1 com valor de x1 em float
 and t4,t4,t1 #t4 com valor de y1
 fcvt.s.w f2,t4 #f2 com valor de y1 em float
-#C�digo que faz o calculo do modulo
+#Código que faz o calculo do modulo
 fmul.s f1,f1,f1
 fmul.s f2,f2,f2
 fadd.s f3,f2,f1
-fsqrt.s f4,f3 # Colocamos o valor do m�dulo em f4
+fsqrt.s f4,f3 # Colocamos o valor do módulo em f4
 addi s0,t2,96 # Valor para o vetor de modulos
 fsw f4,0(s0)
 addi t2,t2,4
@@ -52,11 +52,11 @@ addi t0,t0,1
 blt t0,a2,VECTOR
 addi a4,a2,-1 #Calcula o valor de N-1
 mul a3,a4,t5 #multiplicamos o (N-1)x4
-sub s0,s0,a3 #  voltando ao inicio do vetor de tamanho (N-1)[o primeiro n�o tem mais!] ( t4 = (N-1)x4)
+sub s0,s0,a3 #  voltando ao inicio do vetor de tamanho (N-1)[o primeiro não tem mais!] ( t4 = (N-1)x4)
 
-##Codigo que faz a ordena��o do vetor de m�dulos
+##Codigo que faz a ordenação do vetor de módulos
 
-MAIN:	mv a0,s0 #mudei aqui para ir para nosso inicio do VETOR DE MODULOS ( cuidado para n�o enviar a word mas o endere�o)
+MAIN:	mv a0,s0 #mudei aqui para ir para nosso inicio do VETOR DE MODULOS ( cuidado para não enviar a word mas o endereço)
 	li a1,N
 	jal SHOW
 
@@ -118,7 +118,7 @@ SHOW:	mv t0,a0
 	mv t2,zero
 
 loop1: 	beq t2,t1,fim1
-	li a7,2 #alterei aqui para li a7, 2 que � imprimir FLOAT
+	li a7,2 #alterei aqui para li a7, 2 que é imprimir FLOAT
 	flw fa0,0(t0) # coloquei aqui para float
 	ecall
 	li a7,11
