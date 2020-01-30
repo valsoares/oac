5a62ad326a985e5fdc6a93f2dc7e58e5ed1c5643
Kakomo <kakomo2@gmail.com>
2020-01-30 07:22:09 -0500

    Update poli.asm

--- poli.asm
+++ poli.asm
@@ -1,14 +1,11 @@
-.eqv N 3
-.eqv A 7
-
 .data
 coordenadas: .word
 
 .text
-li a2,N
-la t2,coordenadas #peguei o endere√ßo do vetor onde vou guardar as words coordenadas
-sw a2,0(t2) #primeiro elemento √© o n√∫mero de elementos no vetor coordenadas
-li a3,A
+li a2,3
+la t2,coordenadas #peguei o endere√É¬ßo do vetor onde vou guardar as words coordenadas
+sw a2,0(t2) #primeiro elemento √É¬© o n√É¬∫mero de elementos no vetor coordenadas
+li a3,7
 sw a3,512(t2)
 
 REPETE:
@@ -46,20 +43,26 @@ lw a2,0(t2)
 #SORTEIA(N)
 
 sorteia:
-li a7,42 #gera um inteiro aleatorio
-li a1,319 #limite superior da escolha do inteiro aleatorio de x
+li a7,41 #gera um inteiro aleatorio
+#li a1,319 #limite superior da escolha do inteiro aleatorio de x
 ecall #fez o random do x em a0. Ex: 0x00004444
+li t3,310
+remu a0,a0,t3
+
 slli t1,a0,16 #levar o numero para os 4 primeiros numeros de uma word. Ex: 0x44440000
 
-li a1,239 #limite superior da escolha do inteiro aleatorio de y
+#li a1,239 #limite superior da escolha do inteiro aleatorio de y
 ecall #fez o random do y em a0. Ex: 0x00005555
-or a0,a0,t1 #opera√ß√£o or(x+y) em a0. Ex: 0x44445555
+li t3,230
+remu a0,a0,t3
+
+or a0,a0,t1 #opera√É¬ß√É¬£o or(x+y) em a0. Ex: 0x44445555
 sw a0,4(t2) #armazenamento da coordenada em uma word
 addi t2,t2,4 #incrementa 4 em t2 para o armazenamento da proxima word
 addi t0,t0,1 #incrementa contador de numeros sorteados
-blt t0,a2,sorteia #t0 √© o contador de numeros sorteados: if(t0<N) 
+blt t0,a2,sorteia #t0 √É¬© o contador de numeros sorteados: if(t0<N) 
 
-#Os vetores sorteados est„o salvos na memoria 10010000 
+#Os vetores sorteados est√£o salvos na memoria 10010000 
 
 #CODIGO XANDINHO
 
@@ -68,7 +71,7 @@ mul a3,a2,t5 #multiplicamos o Nx4
 sub t2,t2,a3 #  voltando ao inicio do vetor de tamanho N ( t4 = Nx4)
 mv t0,zero
 mv t6,t2
-#Codigo que faz a serapaÁ„o de x e y no vetor principal
+#Codigo que faz a serapa√ß√£o de x e y no vetor principal
 VECTOR:
 lw t1,4(t2) #primeira coordenada
 li t3, 0xFFFF0000 #mascara do x
@@ -80,11 +83,11 @@ fcvt.s.w f1,t3 #f1 com valor de x1 em float
 and t4,t4,t1 #t4 com valor de y1
 sw t4,260(t6) #salvar y na memoria para mais tarde
 fcvt.s.w f2,t4 #f2 com valor de y1 em float
-#CÛdigo que faz o calculo do modulo
+#C√≥digo que faz o calculo do modulo
 fmul.s f1,f1,f1
 fmul.s f2,f2,f2
 fadd.s f3,f2,f1
-fsqrt.s f4,f3 # Colocamos o valor do mÛdulo em f4
+fsqrt.s f4,f3 # Colocamos o valor do m√≥dulo em f4
 addi s0,t2,96 # Valor para o vetor de modulos
 fsw f4,0(s0)
 addi t2,t2,4
@@ -93,13 +96,13 @@ addi t0,t0,1
 blt t0,a2,VECTOR
 addi a4,a2,-1 #Calcula o valor de N-1
 mul a3,a4,t5 #multiplicamos o (N-1)x4
-sub s0,s0,a3 #  voltando ao inicio do vetor de tamanho (N-1)[o primeiro n„o tem mais!] ( t4 = (N-1)x4)
-##Codigo que faz a ordenaÁ„o do vetor de mÛdulos
+sub s0,s0,a3 #  voltando ao inicio do vetor de tamanho (N-1)[o primeiro n√£o tem mais!] ( t4 = (N-1)x4)
+##Codigo que faz a ordena√ß√£o do vetor de m√≥dulos
 
-MAIN:	mv a0,s0 #mudei aqui para ir para nosso inicio do VETOR DE MODULOS ( cuidado para n„o enviar a word mas o endereÁo)
+MAIN:	mv a0,s0 #mudei aqui para ir para nosso inicio do VETOR DE MODULOS ( cuidado para n√£o enviar a word mas o endere√ßo)
 	la a1, coordenadas
 	lw a1,0(a1)
-	jal SHOW
+	#jal SHOW
 
 	mv a0,s0 #aqui tambem
 	la a1, coordenadas
@@ -110,7 +113,7 @@ MAIN:	mv a0,s0 #mudei aqui para ir para nosso inicio do VETOR DE MODULOS ( cuida
 	mv a0,s0 #aqui mais uma vez...
 	la a1, coordenadas
 	lw a1,0(a1)
-	jal SHOW
+	#jal SHOW
 	jal POLIGONO
 	
 
@@ -123,8 +126,8 @@ SWAP:	slli t1,a1,2
 	add t6,a0,t6
 	lw t0,0(t1) #carrega o modulo da primeira coordenada
 	lw t2,4(t1) # carrega o modulo da segunda coordenada
-	sw t2,0(t1) #salva o modulo da segunda coordenada no primeiro espaÁo
-	sw t0,4(t1) #salva o modulo da primeira coordenada no segundo espaÁo
+	sw t2,0(t1) #salva o modulo da segunda coordenada no primeiro espa√ßo
+	sw t0,4(t1) #salva o modulo da primeira coordenada no segundo espa√ßo
 	lw t0,0(t6) # carrega o x0
 	lw t2,8(t6) # carrega o x1
 	sw t2,0(t6) # salva o x1 no local do x0
@@ -167,12 +170,12 @@ exit1: 	lw s0,0(sp)
 	addi sp,sp,20
 	ret
 
-SHOW:	mv t0,a0
-	mv t1,a1
-	mv t2,zero
+#SHOW:	mv t0,a0
+#	mv t1,a1
+#	mv t2,zero
 
 loop1: 	beq t2,t1,fim1
-	li a7,2 #alterei aqui para li a7, 2 que È imprimir FLOAT
+	li a7,2 #alterei aqui para li a7, 2 que √© imprimir FLOAT
 	flw fa0,0(t0) # coloquei aqui para float
 	ecall
 	li a7,11
@@ -187,27 +190,28 @@ fim1:	li a7,11
 	ecall
 	ret
 
-	#Os Vetore Ordenados est„o salvos em:
+	#Os Vetore Ordenados est√£o salvos em:
 		#Vetor de Coordenadas desordenas: 10010000
-		#Vetor de MÛdulos Ordenados: 10010060
-		#Vetor de Coordenadas SEPARADAS EM X e Y, ORDENADOS em relaÁ„o aos MODULOS : 10010100
+		#Vetor de M√≥dulos Ordenados: 10010060
+		#Vetor de Coordenadas SEPARADAS EM X e Y, ORDENADOS em rela√ß√£o aos MODULOS : 10010100
+	
 	#CODIGO KAKOMO
 	
-POLIGONO:#funÁ„o que coloca V(coordenadas x e y) e A(cor)
- 	la t0,exceptionHandling	# carrega em tp o endereÁo base das rotinas do sistema ECALL
- 	csrw t0,utvec 		# seta utvec para o endereÁo tp
- 	csrwi ustatus,1 	# seta o bit de habilitaÁ„o de interrupÁ„o em ustatus (reg 0)
+POLIGONO:#fun√ß√£o que coloca V(coordenadas x e y) e A(cor)
+ 	la t0,exceptionHandling	# carrega em tp o endere√ßo base das rotinas do sistema ECALL
+ 	csrw t0,utvec 		# seta utvec para o endere√ßo tp
+ 	csrwi ustatus,1 	# seta o bit de habilita√ß√£o de interrup√ß√£o em ustatus (reg 0)
  	
-#Preciso manter os registradores a1-->a5 vazios para colocar as funÁıes
+#Preciso manter os registradores a1-->a5 vazios para colocar as fun√ß√µes
 
-	la t2,coordenadas # pegando endereÁo do Vetor de coordenadas aleatorias
-	addi t2,t2,256 # Colocando o endereÁo do Vetor de Coordenadas x e y ORDENADAS
+	la t2,coordenadas # pegando endere√ßo do Vetor de coordenadas aleatorias
+	addi t2,t2,256 # Colocando o endere√ßo do Vetor de Coordenadas x e y ORDENADAS
 	la a4, coordenadas
 	lw a4,512(a4) #salvando cor em 10010200
-	# lw t3,0(coordenadas) #pegar o valor de N l· na memÛria, primeiro vetora da coordenadas
+	# lw t3,0(coordenadas) #pegar o valor de N l√° na mem√≥ria, primeiro vetora da coordenadas
 	la t3, coordenadas
 	lw t3,0(t3) # numero de pontos
-	addi t3,t3,-1	# (N-1), È o numero de retas que devem ser feitass	
+	addi t3,t3,-1	# (N-1), √© o numero de retas que devem ser feitass	
 	mv t4,zero  #zerando t4 para que o loop nao pegar lixo
 LINHA: 	
 	lw s0,0(t2) #colocando em um registrador o valor do primeiro x
@@ -230,8 +234,8 @@ ULTIMALINHA:
 	mv a0,s0 #pega o valor de x0 e poe em a0
 	lw s0,4(t2)
 	mv a1,s0 #pega o valor de y0 e poe em a1
-	la t2,coordenadas # pegando endereÁo do Vetor de coordenadas aleatorias
-	addi t2,t2,256 # Colocando o endereÁo do Vetor de Coordenadas x e y ORDENADAS
+	la t2,coordenadas # pegando endere√ßo do Vetor de coordenadas aleatorias
+	addi t2,t2,256 # Colocando o endere√ßo do Vetor de Coordenadas x e y ORDENADAS
 	lw s0,0(t2) #colocando em um registrador o valor do primeiro x
 	mv a2,s0 #pega o valor de x1 e poe em a0
 	lw s0,4(t2)
